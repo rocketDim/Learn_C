6da028efc72365ffd40b1023eb47ca441a686138
dem <dmitriydem81@mail.ru>
2022-01-22 21:08:25 +0300

    Массив ч.3 (Чет, Нечет, Случайные числа)

---
+++ array3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int N = 6;
+
+int main()
+{
+
+    int arr[N];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = rand() % 41;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int chet = 0;
+    int nech = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            chet++;
+        }
+
+        if (arr[i] % 2 != 0)
+        {
+            nech++;
+        }
+    }
+    printf("Четные: %d\nНечетные: %d\n",  chet, nech);
+
+    return 0;
+}
