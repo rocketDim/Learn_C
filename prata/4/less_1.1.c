f560bfc407c1357461371c1ffd98d1c46476807a
dem <dmitriydem81@mail.ru>
2022-03-12 20:20:47 +0300

    Готово

---
+++ prata/4/less_1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+char name[40];
+char sur[40];
+printf("Kaк вас зовут? ");
+scanf ("%s %s", name, sur);
+printf("Здравствуйте, %s, %s\n", sur,name); 
+
+return 0;
+}
