e3b8203a71d5f9840a2315db128999b219c49915
dem <dmitriydem81@mail.ru>
2022-03-13 01:24:30 +0300

    Проблема с выравниванием

---
+++ prata/2/vv.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define PI 3.14159
+
+#include <stdio.h>
+#include <string.h>
+#define DENSITY 62.4 
+
+int main()
+{
+    float weight, volume;
+    int size, letters;
+    char name[40];
+    printf("Здравствуйте! Как вас зовут?\n");
+    scanf("%s", name);
+    printf("%s, сколько вы весите в фунтах? \n", name);
+    scanf("%f", &weight);
+    size = sizeof name;
+    letters = strlen(name);
+    volume = weight / DENSITY;
+    printf("Хорошо, %s, ваш объем составляет %2.2f кубических футов.\n",
+           name, volume);
+    printf("К тому же ваше имя состоит из %d букв,\n",
+           letters);
+    printf("и мы располагаем 40 байтами для его сохранения.\n", size);
+    return 0;
+}
