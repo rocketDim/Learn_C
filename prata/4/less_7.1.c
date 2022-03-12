2cc61044b459baf75126d6a39b23477ee60abfa3
dem <dmitriydem81@mail.ru>
2022-03-13 02:33:18 +0300

    Готово

---
+++ prata/4/less_7.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    double m = 1.0 / 3.0;
+    float d = 1.0 / 3.0;
+    printf("Double: %.4f, %.12f, %.16f\n", m, m, m);
+    printf("Float:  %.4f, %.12f, %.16f\n", d, d, d);
+    printf("Точность double: %d знаков\n", DBL_DIG);
+    printf("Точность float: %d знаков\n", FLT_DIG);
+    
+
+    return 0;
+}
