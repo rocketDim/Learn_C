857de7bbb77f77d2823752c8e3bfb709274600e2
dem <dmitriydem81@mail.ru>
2022-03-13 17:14:44 +0300

    Исправлено

--- prata/4/less_5.1.c
+++ prata/4/less_5.1.c
@@ -2,17 +2,18 @@
 
 int main(void)
 {
+    float speed, size;
 
-float speed, size;
+    printf("Скорость загрузки (Mbit/sec): ");
+    scanf("%f", &speed);
 
-printf("Скорость загрузки (Mbit/sec): ");
-scanf("%f", &speed);
-printf("Размер файла (Mbyte): ");
-scanf("%f", &size);
+    printf("Размер файла (Mbyte): ");
+    scanf("%f", &size);
 
+    printf("При скорости загрузки %.2f"
+           " мегабит в секунду файл размером %.2f"
+           " мегабайт загружается за %.2f секунд(ы)\n",
+           speed, size, (size / (speed / 8)));
 
-
-    
-    
     return 0;
 }
