300e319315e0baa12e0dbad61d2c81337be9201f
dem <dmitriydem81@mail.ru>
2022-04-15 10:52:31 +0300

    3 константы

--- prata/5/lesson_9.c
+++ prata/5/lesson_9.c
@@ -4,12 +4,11 @@ int temperatures(double far)
 {
     double cel, kel;
     double const TEM_1 = 32.0;
-    double const TEM_2 = 5.0;
-    double const TEM_3 = 9.0;
-    double const TEM_4 = 273.16;
+    double const TEM_2 = 5.0 / 9.0;
+    double const TEM_3 = 273.16;
 
-    cel = TEM_2 / TEM_3 * (far - TEM_1);
-    kel = cel + TEM_4;
+    cel = TEM_2 * (far - TEM_1);
+    kel = cel + TEM_3;
 
     printf("Фаренгейт F: %8.2lf\nЦельсий C: %10.2lf\nКельвин K: %10.2lf\n", far, cel, kel);
 
