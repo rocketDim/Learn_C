6aecfa5f24aea175c6ef2624b8f91cde7a604c10
dem <dmitriydem81@mail.ru>
2022-04-26 00:31:06 +0300

    YES

---
+++ TASKS/les_4.c
@@ -0,0 +1,21 @@
+// Простейшая программа с функциями
+#include <stdio.h>
+void jolly(void)
+{
+    printf("Он веселый молодец!\n");
+}
+
+
+void denny(void)
+{
+    printf("Никто не может это отрицать!\n");
+}
+
+int main(void)
+{
+    jolly();
+    jolly();
+    jolly();
+    denny();
+    return 0;
+}
