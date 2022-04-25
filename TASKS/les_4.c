b2d5cf1820a233bc9ebb712110f0a8c16ebf6977
dem <dmitriydem81@mail.ru>
2022-04-26 01:05:30 +0300

    Добавлены комантарии

--- TASKS/les_4.c
+++ TASKS/les_4.c
@@ -1,21 +1,22 @@
 // Простейшая программа с функциями
 #include <stdio.h>
-void jolly(void)
-{
-    printf("Он веселый молодец!\n");
-}
-
-
-void denny(void)
-{
-    printf("Никто не может это отрицать!\n");
-}
-
+void jolly(void); // прототип функции jolly
+void denny(void); // прототип функции denny
 int main(void)
 {
-    jolly();
+    jolly(); // вызов функции
     jolly();
     jolly();
     denny();
     return 0;
+}
+
+void jolly(void) // функция jolly
+{
+    printf("Он веселый молодец!\n");
+}
+
+void denny(void) // функция denny
+{
+    printf("Никто не может это отрицать!\n");
 }
