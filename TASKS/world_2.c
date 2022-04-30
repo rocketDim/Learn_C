29bf6a804a3fc246046a24516f7c7ec5b0df19ff
dem <dmitriydem81@mail.ru>
2022-04-30 21:32:23 +0300

    ширина/высота

---
+++ TASKS/world_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    int height = 1;
+    while(height <= 3)
+    {
+        printf("x\n");
+        height++;
+    }
+    int wight = 1;
+    while(wight <= 8)
+    {
+        printf(" ");
+        wight++;
+    }
+
+
+    return 0;
+}
