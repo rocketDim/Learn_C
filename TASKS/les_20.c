d89b6c9afa79601c458cb3893d9fad53f2d60f75
dem <dmitriydem81@mail.ru>
2022-05-23 20:39:40 +0300

    YES

---
+++ TASKS/les_20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    float growth;
+    char name[40];
+    printf("Введите рост в см и имя: ");
+    scanf("%s %f", name, &growth);
+    printf("%s, ваш рост составляет %.2f метра.\n", name, growth / 100);
+    return 0;
+}
