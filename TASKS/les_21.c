56b29f3e1f7a4e78d0c41c2f3d6f0f774c3c93d9
dem <dmitriydem81@mail.ru>
2022-05-23 20:52:40 +0300

    YES

---
+++ TASKS/les_21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    float speed, size;
+    float result;
+    printf("Введите скорость загрузки и размер файла: ");
+    scanf("%f %f", &speed, &size);
+    result = size / 8;
+    printf("При скорости загрузки %.2f "
+           "мегабит/сек файл размером %.2f "
+           "загружается за %.2f секунд",
+           speed, result, (speed / result));
+
+    return 0;
+}
