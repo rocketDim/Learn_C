3fdf67ae3168222d2f206d68583393a048456579
dem <dmitriydem81@mail.ru>
2022-05-25 22:20:51 +0300

    YES

---
+++ TASKS/les_27.c
@@ -0,0 +1,20 @@
+// Программа преобразует дни в недели и дни
+#include <stdio.h>
+const int DAY_IN_WEEK = 7;
+int main(void)
+{
+    int days, week, num;
+    printf("Введите кол-во дней: ");
+    scanf("%d", &days);
+    while (num > 0)
+    {
+        week = days % DAY_IN_WEEK;
+        num = days / DAY_IN_WEEK;
+        printf("%d дней состовляют %d недели и %d дня.\n", days, week, num);
+
+        printf("Введите следующее кол-во дней: ");
+        scanf("%d", &days);
+    }
+
+    return 0;
+}
