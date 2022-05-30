e1751d3368cf03dec34d773c9d0a5bbbbe96e9fc
dem <dmitriydem81@mail.ru>
2022-05-30 19:23:47 +0300

    YES

---
+++ TASKS/array_4.c
@@ -0,0 +1,21 @@
+//Задача на ввод/вывод в массив
+#include <stdio.h>
+int main(void)
+{
+    const int N = 3;
+    int arr[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Введите %d из %d: ", i, N);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
